420a70042011f7d9697d94e91d67a04fdcf54bed
ricardocollado <ricardocolladorothschild@gmail.com>
2023-09-05 12:55:59 -0400

    crate method addNewNote

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+const mainContainer = document.querySelector('.main-container');
+//boton section
+const btnAdd = document.querySelector('.button-addNew');
+
+btnAdd.addEventListener('click',(event)=>{
+    event.preventDefault();
+    addNewNote();
+});
+
+
+
+
+function addNewNote(){
+    const mainNoteContainer = document.createElement('div');
+    mainNoteContainer.classList.add('main-note--container');
+
+    mainNoteContainer.innerHTML = `
+    <div class="note-container--tools">
+    <button class="tools--edit">Edit</button>
+    <button class="tools--delete">Delete</button>
+    </div>
+    <textarea class="note-container--textArea"></textarea>
+    `;
+
+    mainContainer.append(mainNoteContainer);
+}
+
